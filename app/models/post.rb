b4b08f1224f93d8970a7a8536eda94cd22937963
Connor Vosberg <connorjv@gmail.com>
2023-07-26 13:59:39 -0700

    commit tree d17d998616dbd6cd88ad47dc93552fa4c7819d82

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait?
+
+    #regex patterns for clickbait?
+    
+
+    def clickbait?
+        
+        clickbait_patterns = [
+            /Won't Believe/i,
+            /Secret/i,
+            /Top \d/i,
+            /Guess/i
+        ]
+
+        if clickbait_patterns.any? { |pattern| pattern.match title}
+        else
+            errors.add(:title, "must be clcikbait")
+        end
+    end
+
+
 end
